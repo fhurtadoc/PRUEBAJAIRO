4410936783fe69512c3df096ea037d42dd91f225
Jonathan <jonathanguillermojm@ufps.edu.co>
2021-03-04 21:10:41 -0500

    Revert "prueba"
    This reverts commit c12b2494adab7c940c91b9c4adbf44f2eeaa4cb5.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 fretchNumber()
 
-const numeroActual = "556"
-
 setInterval(function(){
     $(".numero_img").remove();
     fretchNumber();    
@@ -10,76 +8,64 @@ setInterval(function(){
 
 function fretchNumber(){
     $.get("list_numero.php", function(res, err){
-        let numero=JSON.parse(res);
-        
+        let numero=JSON.parse(res);        
+        for(let un_numero of numero.numero){            
+        const newDiv=document.createElement("img");         
+        let continer=document.querySelector("#continer").append(newDiv);         
+            switch(un_numero){                
+                case "0":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/0c.png");
+                break    
 
-        
-        if (numeroActual != numero) {
-            
-            for(let un_numero of numero.numero){            
-                const newDiv=document.createElement("img");         
-                let continer=document.querySelector("#continer").append(newDiv);  
-        
-                     
-                        
-        
-                    switch(un_numero){                
-                        case "0":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/0c.png");
-                        break    
-        
-                        case "1":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/1c.png");
-                        break    
-        
-                        case "2":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/2c.png");
-                        break    
-        
-                        case "3":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/3c.png");
-                        break    
-        
-                        case "4":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/4c.png");
-                            
-                        break    
-        
-                        case "5":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/5c.png");
-                        break    
-        
-                        case "6":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/6c.png");
-                        break    
-        
-                        case "7":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/7c.png");
-                        break    
-        
-                        case "8":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/8c.png");
-                        break    
-        
-                        case "9":
-                            newDiv.setAttribute ("class", "numero_img");
-                            newDiv.setAttribute ("src", "numerosimg/9c.png");
-                        break    
-                    }
-        
-                }
+                case "1":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/1c.png");
+                break    
+
+                case "2":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/2c.png");
+                break    
+
+                case "3":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/3c.png");
+                break    
+
+                case "4":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/4c.png");
+                    
+                break    
+
+                case "5":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/5c.png");
+                break    
+
+                case "6":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/6c.png");
+                break    
+
+                case "7":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/7c.png");
+                break    
+
+                case "8":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/8c.png");
+                break    
+
+                case "9":
+                    newDiv.setAttribute ("class", "numero_img");
+                    newDiv.setAttribute ("src", "numerosimg/9c.png");
+                break    
+            }
+
         }
-
-        
         
     });
 
